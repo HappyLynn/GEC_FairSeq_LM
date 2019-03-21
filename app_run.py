4732174a823788d6dddbab84b025a30ca0866af1
Lynn <cwangzhe@126.com>
2019-03-21 14:55:04 +0800

    Update app_run.py

--- app_run.py
+++ app_run.py
@@ -3,7 +3,12 @@
 import sys
 import glob
 import os
-
+import spacy
+from contextlib import ExitStack
+from nltk.stem.lancaster import LancasterStemmer
+import scripts.align_text as align_text
+import scripts.cat_rules as cat_rules
+import scripts.toolbox as toolbox
 # with open('/data/wangzhe/SematicSeg/mlconvgec2018/models/data_bin/dict.trg.txt', 'r') as in_f:
 #     with open('/data/wangzhe/SematicSeg/mlconvgec2018/models/data_bin/dict.load.txt', 'w') as out_f:
 #         for text in in_f.readlines():
@@ -48,7 +53,17 @@ weights = [0.94064, -0.0208803, -0.00450021, 0.015532, 0.00618153, -0.0122658]
 models, generator, align_dict, max_positions, args, use_cuda, task, src_dict, tgt_dict = load(model_names=model_names,
                                                                                               use_cpu=False)
 
+basename = os.path.dirname(os.path.realpath(__file__))
 
+# Load Tokenizer and other resources
+nlp = spacy.load("en")
+# Lancaster Stemmer
+stemmer = LancasterStemmer()
+# GB English word list (inc -ise and -ize)
+gb_spell = toolbox.loadDictionary('/data/wangzhe/SematicSeg/mlconvgec2018/models/data_bin/dict.load.txt')
+# Part of speech map file
+tag_map = toolbox.loadTagMap(basename+"/resources/en-ptb_map")
+print("Loading resources...")
 def cross_make_response(json_info):
     response = make_response(json_info)
     response.headers['Content-Type'] = 'text/html'
@@ -65,7 +80,7 @@ def index():
 
 
 @app.route('/', methods=['GET', 'POST'])
-def get_tts_preprocess_text():
+def get_preprocess_text():
     if request.method == 'POST':
         in_str = request.form.get("sen_input")
         input_sen = in_str#"If your genetic results indicate that you have gene changes associated with an increased risk of heart disease , it does not mean that you definitely will develop heart disease ."
@@ -105,9 +120,31 @@ def get_tts_preprocess_text():
             score_dict[ind] = final_score
             print(s0, s1[0], s1[1], s1[2], s2, s3)
         sorted_indices = sorted(score_dict, key=score_dict.get, reverse=True)
+        out_type = []
         for ind in sorted_indices:
+            proc_orig = toolbox.applySpacy(input_sen.split(), nlp)
+            output_type = '\n'
+            cor_sent = outputs[ind]
+            cor_sent = cor_sent.strip()
+            # Identical sentences have no edits, so just write noop.
+            if input_sen == cor_sent:
+                output_type += "A -1 -1|||noop|||-NONE-|||REQUIRED|||-NONE-|||" + "\n"
+            # Otherwise, do extra processing.
+            else:
+                # Markup the corrected sentence with spacy (assume tokenized)
+                proc_cor = toolbox.applySpacy(cor_sent.strip().split(), nlp)
+                # Auto align the parallel sentences and extract the edits.
+                auto_edits = align_text.getAutoAlignedEdits(proc_orig, proc_cor, nlp, True, 'rules')
+                # Loop through the edits.
+                for auto_edit in auto_edits:
+                    # Give each edit an automatic error type.
+                    cat = cat_rules.autoTypeEdit(auto_edit, proc_orig, proc_cor, gb_spell, tag_map, nlp, stemmer)
+                    auto_edit[2] = cat
+                    # Write the edit to the output m2 file.
+                    output_type += toolbox.formatEdit(auto_edit, 0) + "\n"
+            out_type.append(output_type)
             print(outputs[ind])
-        couplet_res = outputs[sorted_indices[0]]
+        couplet_res = outputs[sorted_indices[0]] + out_type[0]
         sys.stdout.flush()
         return render_template('show.html', sen_input=input_sen, sen_res=couplet_res)
     else:
